5fd8d1e366c79450446a2f8459d025655ce810fc
Tristan <deaugustinetristan@gmail.com>
2019-08-07 10:10:52 -0400

    intervals

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+const main = () => {}
 
+const colorChange = () => {
+  let rgbColor
+  red = Math.floor(Math.random() * 250)
+  green = Math.floor(Math.random() * 250)
+  blue = Math.floor(Math.random() * 250)
+
+  rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')'
+  document.body.style.background = rgbColor
+
+  red = ('0' + red.toString(16)).substr(-2).toUpperCase()
+  green = ('0' + green.toString(16)).substr(-2).toUpperCase()
+  blue = ('0' + blue.toString(16)).substr(-2).toUpperCase()
+
+  setInterval(function() {
+    colorChange('Hello')
+  }, 3000)
+}
+colorChange()
+
+document.addEventListener('click', colorChange)
 document.addEventListener('DOMContentLoaded', main)
